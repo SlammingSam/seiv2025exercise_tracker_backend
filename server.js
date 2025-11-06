185e2190ded7b14ff7bb5afec1fabf608309b3b0
joolean-equals-true <160288072+joolean-equals-true@users.noreply.github.com>
2025-11-05 23:22:39 -0600

    fffff

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
   
 // Load the routes from the routes folder
-app.use("/tracker-4", routes); 
+app.use("/tracker-t4", routes); 
 
 
 // set port, listen for requests
